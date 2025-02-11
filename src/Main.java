d17256c57a27a3581c5212fd45e799f00348ce14
NurD891 <nurdanabdyganiev58@gmail.com>
2025-02-11 23:06:06 +0600

    change 2

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
 import java.security.SecureRandom;
 import java.util.Scanner;
 
+/**
+ * PasswordGenerator class to generate secure passwords.
+ * Allows users to specify length with a minimum of 6 characters.
+ */
 public class Main {
     private static final String CHARACTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_+=<>?/";
 
+    /**
+     * Generates a random password of the given length.
+     * @param length Desired length of the password (minimum 6).
+     * @return Generated password as a string.
+     */
     public static String generatePassword(int length) {
         SecureRandom random = new SecureRandom();
         StringBuilder password = new StringBuilder();
@@ -13,6 +22,9 @@ public class Main {
         return password.toString();
     }
 
+    /**
+     * Main method to interact with the user and generate a password.
+     */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter desired password length:");
